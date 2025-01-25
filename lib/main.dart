5d322ef264f81e1dcf9461202bacb1e9182f4666
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2025-01-25 23:07:14 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
   List<ChatModel> chatList = [
   ]; // Your list of ChatModel objects
